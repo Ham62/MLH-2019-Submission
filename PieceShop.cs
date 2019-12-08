9d3bf5033e9a4091d9c54e836e5a73ea8d9ed35a
Graham Downey <grahamdowney1@gmail.com>
2019-12-07 19:20:04 -0600

    Add files via upload

--- PieceShop.cs
+++ PieceShop.cs
@@ -15,16 +15,25 @@ namespace FBGFXDemo
 		}
 
 		readonly shopItem[] shopItems = new shopItem[9];//6 items in chess
-
-		const int EXIT = 7, BUY = 8;//pos in shopItems
-
+        static fbgfx.IMAGE*[] images = new fbgfx.IMAGE*[9];
 
 		public readonly int menuWidth, menuHeight;
 		public readonly Pos menuPosition;
+        Region menuArea;
 		public readonly uint menuColor;
 		private fbgfx.IMAGE* menu;
 		public bool isShowing = false;
+        int selectedItem;
 
+        const int MB_SHOP = 0,
+                  MB_PAWN = 1,
+                  MB_KNIGHT = 2,
+                  MB_KING = 3,
+                  MB_BISHOP = 4,
+                  MB_ROOK = 5,
+                  MB_QUEEN = 6,
+                  MB_EXIT = 7,
+                  MB_BUY = 8;
 
 		public PieceShop(String imgPath, int cost, Player owner, int menuWidth, int menuHeight, Pos menuPosition,uint menuColor) : base(imgPath,cost,owner,MOVEMODE.NONE)
         {
@@ -32,6 +41,9 @@ namespace FBGFXDemo
 			this.menuWidth = menuWidth;
 			this.menuHeight = menuHeight;
 			this.menuPosition = menuPosition;
+            menuArea = new Region(menuPosition.x, menuPosition.y, 
+                                  menuPosition.x + menuWidth, 
+                                  menuPosition.y + menuHeight);
 
 			//init buttons
 			for(int i =0; i < shopItems.Length; i++)
@@ -40,19 +52,27 @@ namespace FBGFXDemo
 				shopItems[i].button = new GameButton(0,0,0,0);
 			}
 
-			shopItems[0].item = new Piece("shop.png", 2, owner, MOVEMODE.NONE);
-			shopItems[1].item = new Piece("pawn.png", 1, owner, MOVEMODE.PAWN);
-			shopItems[2].item = new Piece("knight.png",3,owner,MOVEMODE.KNIGHT);
-			shopItems[3].item = new Piece("king.png",4,owner,MOVEMODE.KING);
-			shopItems[4].item = new Piece("bishop.png",10,owner,MOVEMODE.BISHOP);
-			shopItems[5].item = new Piece("rook.png", 10,owner,MOVEMODE.ROOK);
-			shopItems[6].item = new Piece("queen.png",15,owner,MOVEMODE.QUEEN);
-			shopItems[EXIT].item = new Piece("",0,null,MOVEMODE.NONE);
-			shopItems[BUY].item = new Piece("", 0, null, MOVEMODE.NONE);
+			shopItems[MB_SHOP].item = new Piece("res/shop.png", 2, owner, MOVEMODE.NONE);
+            shopItems[MB_PAWN].item = new Piece("res/pawn.png", 1, owner, MOVEMODE.PAWN);
+            shopItems[MB_KNIGHT].item = new Piece("res/knight.png", 3, owner, MOVEMODE.KNIGHT);
+            shopItems[MB_KING].item = new Piece("res/king.png", 4, owner, MOVEMODE.KING);
+            shopItems[MB_BISHOP].item = new Piece("res/bishop.png", 10, owner, MOVEMODE.BISHOP);
+            shopItems[MB_ROOK].item = new Piece("res/rook.png", 10, owner, MOVEMODE.ROOK);
+            shopItems[MB_QUEEN].item = new Piece("res/queen.png", 15, owner, MOVEMODE.QUEEN);
 
-			updateMenuImage();
-			shopItems[EXIT].button.clickedAction = exitFunction;
 
+            for (int i = 0; i < shopItems.Length; i++)
+            {
+                if (shopItems[i].item != null)
+                {
+                    shopItems[i].button.clickedAction = itemButtonClicked;
+                    images[i] = fbgfx.LoadPNG(shopItems[i].item.imgPath);
+                }
+            }
+
+                updateMenuImage();
+			shopItems[MB_EXIT].button.clickedAction = exitFunction;
+            shopItems[MB_BUY].button.clickedAction = buyClicked;
 
 		}
 
@@ -66,14 +86,14 @@ namespace FBGFXDemo
 
 			for(int i =0; i < shopItems.Length; i++)
 			{
-				if(shopItems[i].item.imgPath != "")
+				if(shopItems[i].item != null && shopItems[i].item.imgPath != "")
 				{
 					shopItems[i].button.x = 0;
 					shopItems[i].button.y = i * buttonSize;
 					shopItems[i].button.height = buttonSize;
 					shopItems[i].button.width = buttonSize* btnScale;
 
-					String imgName = shopItems[i].item.imgPath.Substring(0, shopItems[i].item.imgPath.Length - 4);
+					String imgName = shopItems[i].item.imgPath.Substring(4, shopItems[i].item.imgPath.Length - 8);
 					shopItems[i].button.caption = imgName +" - $" + shopItems[i].item.cost;
 
 				}
@@ -81,15 +101,24 @@ namespace FBGFXDemo
 
 			int optBtnSize = shopItems[0].button.width/2;//(shopItems.Length-2)*buttonSize
 
-			shopItems[EXIT].button = new GameButton(0, (shopItems.Length - 2) * buttonSize, optBtnSize, menuHeight- (shopItems.Length - 2) * buttonSize);
-			shopItems[EXIT].button.caption = "EXIT";
-			shopItems[BUY].button = new GameButton(optBtnSize, (shopItems.Length - 2) * buttonSize, optBtnSize, menuHeight - (shopItems.Length - 2) * buttonSize);
-			shopItems[BUY].button.caption = "BUY";
+			shopItems[MB_EXIT].button = new GameButton(0, (shopItems.Length - 2) * buttonSize, optBtnSize, menuHeight- (shopItems.Length - 2) * buttonSize);
+			shopItems[MB_EXIT].button.caption = "EXIT";
+			shopItems[MB_BUY].button = new GameButton(optBtnSize, (shopItems.Length - 2) * buttonSize, optBtnSize, menuHeight - (shopItems.Length - 2) * buttonSize);
+			shopItems[MB_BUY].button.caption = "BUY";
 		}
 
-		public void renderBox()
+		public void renderWindow()
 		{
 			fbgfx.Put(menuPosition.x, menuPosition.y, menu);
+
+            if (images[selectedItem] != null)
+            {
+                int imgSz = menuWidth / 3;
+                fbgfx.ScalePutA(images[selectedItem], 
+                                menuArea.right - menuWidth / 3, 
+                                menuArea.top + menuHeight / 2,
+                                imgSz, imgSz);
+            }
 		}
 
 
@@ -100,6 +129,8 @@ namespace FBGFXDemo
 				WindowControls.Register(shopItems[i].button);
 			}
 		}
+
+        
 
 		GameBoard tempBoard;
         Tile tempTile;
@@ -120,17 +151,62 @@ namespace FBGFXDemo
 
 		private void exitFunction(GameButton btn)
 		{
-			isShowing = false;
-
-			for (int i = 0; i < shopItems.Length; i++)
-			{
-				WindowControls.Unregister(shopItems[i].button);
-			}
-
-			tempBoard.enableBoard(true);
             tempBoard.clearMoveflags();
             tempBoard.selectedTile = null;
             tempTile.isSelected = false;
+
+            terminateWindow();
 		}
+
+        private void itemButtonClicked(GameButton btnClicked)
+        {
+            for (int i = 0; i < shopItems.Length; i++)
+            {
+                if (shopItems[i].button == btnClicked)
+                {
+                    selectedItem = i;
+                    return;
+                }
+            }
+        }
+
+        Tile newTile;
+        private void buyClicked(GameButton btnClicked)
+        {
+            // if have enough gold
+            newTile = new Tile(-1, -1, 0, 0, tempBoard, new ButtonStyle());
+            tempBoard.selectedTile = newTile;
+            newTile.piece = shopItems[selectedItem].item;
+            newTile.imgPath = newTile.piece.imgPath;
+            
+            Pos pos = tempBoard.getCoord(tempTile);
+            for (int i = 0; i < tempBoard.TILES_WIDE; i++)
+            {
+                Tile tmp = tempBoard.tileAt(i, pos.y);
+                if (tmp.piece == null)
+                    tmp.canMove = true;
+            }
+            
+            terminateWindow();
+        }
+
+        private void terminateWindow()
+        {
+            for (int i = 0; i < shopItems.Length; i++)
+            {
+                WindowControls.Unregister(shopItems[i].button);
+            }
+
+            isShowing = false;
+            tempBoard.enableBoard(true);
+
+            for (int i = 0; i < shopItems.Length; i++)
+            {
+                if (images[i] != null)
+                {
+                    fbgfx.DestroyPNG(images[i]);
+                }
+            }
+        }
     }
 }
